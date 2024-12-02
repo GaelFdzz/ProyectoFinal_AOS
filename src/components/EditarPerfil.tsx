15207f19d40dbd3601d82e0be97585c74dd32bee
Gael Fernandez <127038204+GaelFdzz@users.noreply.github.com>
2024-12-02 12:07:50 -0500

    Correccion de front

--- src/components/EditarPerfil.tsx
+++ src/components/EditarPerfil.tsx
@@ -33,54 +33,56 @@ const EditarPerfil: React.FC = () => {
       <div className="container-perfil">
 
         <h1 className="titlePerfil">Editar perfil</h1>
-        <form>
-          <label htmlFor="nombre">Nombre/s</label>
-          <input
-            id="nombre"
-            type="text"
-            name="nombre"
-            value={formData.nombre}
-            onChange={handleChange}
-            placeholder="Ingresa tu nombre(s)"
-          />
+        <div className="formEditarPerfil">
+          <form>
+            <label htmlFor="nombre">Nombre/s</label>
+            <input
+              id="nombre"
+              type="text"
+              name="nombre"
+              value={formData.nombre}
+              onChange={handleChange}
+              placeholder="Ingresa tu nombre(s)"
+            />
 
-          <label htmlFor="apellido">Apellido/s</label>
-          <input
-            id="apellido"
-            type="text"
-            name="apellido"
-            value={formData.apellido}
-            onChange={handleChange}
-            placeholder="Ingresa tu apellido(s)"
-          />
+            <label htmlFor="apellido">Apellido/s</label>
+            <input
+              id="apellido"
+              type="text"
+              name="apellido"
+              value={formData.apellido}
+              onChange={handleChange}
+              placeholder="Ingresa tu apellido(s)"
+            />
 
-          <label htmlFor="correo">Correo</label>
-          <input
-            id="correo"
-            type="email"
-            name="correo"
-            value={formData.correo}
-            onChange={handleChange}
-            placeholder="Ingresa tu correo electrónico"
-          />
+            <label htmlFor="correo">Correo</label>
+            <input
+              id="correo"
+              type="email"
+              name="correo"
+              value={formData.correo}
+              onChange={handleChange}
+              placeholder="Ingresa tu correo electrónico"
+            />
 
-          <div className="button-container">
-            <button
-              type="button"
-              className="button save"
-              onClick={handleGuardarCambios}
-            >
-              Guardar cambios
-            </button>
-            <button
-              type="button"
-              className="button cancel"
-              onClick={handleRegresarAlPerfil}
-            >
-              Regresar al perfil
-            </button>
-          </div>
-        </form>
+            <div className="button-container">
+              <button
+                type="button"
+                className="button-save"
+                onClick={handleGuardarCambios}
+              >
+                Guardar cambios
+              </button>
+              <button
+                type="button"
+                className="button-cancel"
+                onClick={handleRegresarAlPerfil}
+              >
+                Regresar al perfil
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
